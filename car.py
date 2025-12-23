800738a1472788dc2d108eeb77a08d4a831178f0
TMorgan <tm422@student.le.ac.uk>
2025-12-23 14:05:04 +0000

    car optimisation for faster ai

--- car.py
+++ car.py
@@ -1,43 +1,22 @@
-import pygame
+class CarState:
+    """
+    Stores the snapshot of a car at a specific moment (Discrete State).
+    Used for the A* Algorithm to remember where it has been.
+    """
+    def __init__(self, x, y, vx, vy):
+        self.x = x
+        self.y = y
+        self.vx = vx
+        self.vy = vy
 
-class Car:
-    def _init_(self, start_pos, colour=(255, 0, 0)):
-        #Core State
-        self.position = pygame.Vector2(start_pos)
-        self.velocity = pygame.Vector2(0, 0)
-        
-        #Visuals
-        self.colour = colour
-        self.radius = 5
+    #tells Python how to check if two states are the same
+    def __eq__(self, other):
+        return (self.x, self.y, self.vx, self.vy) == (other.x, other.y, other.vx, other.vy)
 
-        #History
-        self.path = [self.position]
-        self.last_safe_position = pygame.Vector2(start_pos)
+    #allows object to be put into a Set or Dictionary
+    def __hash__(self):
+        return hash((self.x, self.y, self.vx, self.vy))
 
-    def update_velocity(self, dx_change, dy_change):
-        """Applies acceleration"""
-        self.velocity.x += dx_change
-        self.velocity.y += dy_change
-    
-    def get_next_position(self):
-        """Calculates where the car WILL GO"""
-        return self.position + self.velocity
-
-    def commit_move(self, new_postion):
-        """Official Car position update"""
-        self.position = pygame.Vector2(new_position)
-        self.path.append(self.position)
-        self.last_safe_position = pygame.Vector2(new_position)  #Stores good move
-    
-    def crash(self):
-        """Resets velocity on crash and moves to last safe spot"""
-        print("CRASH! Velocity reset, back to last safe position")
-        self.velocity = pygame.Vector2(0, 0)
-        self.position = self.last_safe_position  #go back
-        self.path.append(self.position)  #add reset to path
-        
-    def draw(self, screen):
-        #draw car path
-        if len(self.path) > 1:
-            #draw line
-            pygame.draw.lines(screen, self.colour + (100,), False, self.path, 2)
+    #helper for debugging
+    def __repr__(self):
+        return f"Car(x={self.x}, y={self.y}, vx={self.vx}, vy={self.vy})"
